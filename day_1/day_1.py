2905ffe24a41160243449451c336987d6c76da7e
Michael <kilcorse.michael92@gmail.com>
2022-12-01 09:08:58 -0500

    Format decorator

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -4,10 +4,10 @@ FULL_DATA, TEST_DATA = "./day_1/day_1.txt", "./day_1/day_1_min.txt"
 
 def execute_time(func):
 	def wrapper(*args, **kwargs):
-		start = time.time()
+		start = time.process_time()
 		result = func(*args, **kwargs)
-		end = time.time()
-		print(f"{func.__name__}: Ran in {end - start} seconds")
+		end = time.process_time()
+		print(f"{func.__name__}: {result}\nRan in {end - start} seconds")
 		return result
 	return wrapper
 
@@ -24,6 +24,6 @@ def part_two(calories):
 with open(FULL_DATA) as f:
 	calorie_counts = [sum([int(calorie) for calorie in calorie_group.split("\n")]) for calorie_group in f.read().split("\n\n")]
 
-print(part_one(calorie_counts))
-print(part_two(calorie_counts))
+part_one(calorie_counts)
+part_two(calorie_counts)
 
