aa1b70081e4b5b771ba908a83c19ba4919b3b12b
unknown <kilcorse.michael92@gmail.com>
2022-12-02 16:28:52 -0500

    Day 2 Start

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,22 +1,25 @@
 #day_2 AOC 2022 python file
 FULL_DATA, TEST_DATA = "./day_2/day_2.txt", "./day_2/day_2_min.txt"
 
-LOSS_CONDITION = [{"A", "Z"}, {"B", "X"}, {"C", "Y"}]
-DRAW_CONDITION = [{"A", "X"}, {"B", "Y"}, {"C", "Z"}]
-WIN_CONDITION = [{"A", "Y"}, {"B", "Z"}, {"C", "X"}]
+conditions = {"loss": (("A", "Z"), ("B", "X"), ("C", "Y")), "draw":(("A", "X"), ("B", "Y"), ("C", "Z")), "win":(("A", "Y"), ("B", "Z"), ("C", "X"))}
 
 SCORE = 0
 
 with open(TEST_DATA) as f:
-	strategy_sheet = [set(rounds.split()) for rounds in f.read().split("\n")]
+	strategy_sheet = [tuple(rounds.split()) for rounds in f.read().split("\n")]
 
-def find_score(sheet):
-	new_list = []
-	for x in sheet:
-		for y in LOSS_CONDITION:
-			if x != y:
-				new_list.append(x)
-				break
-	return new_list
+win = []
+draw = []
+loss = []
 
-print(find_score(strategy_sheet))
+for i in strategy_sheet:
+	for key in conditions:
+		if i in conditions[key]:
+			eval(key).append(i)
+
+
+print(win, loss, draw)
+
+
+		
+			 
